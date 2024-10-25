3515ccdb5691be9713ad28b42c04262c1122cdfc
Anna-Kuri <nankurenaisa@gmail.com>
2024-10-26 01:22:33 +0300

    module_7_2
    Module 7 Hometask №2

--- module_7_2.py
+++ module_7_2.py
@@ -1,26 +1,22 @@
-def custom_write(file_name: str, strings: list[str]) -> dict:
+def custom_write(file_name: str, strings: list) -> dict:
     strings_position = {}
-
-    file = open(file_name, "a", encoding="utf-8")
-
+    file = open(file_name, 'a', encoding='utf-8')
     for i, string in enumerate(strings, start=1):
         byte_number = file.tell()
-        file.write(f"string\n")
-        strings_position[(i, byte_number)] = string
-
+        file.write(string + '\n')
+        strings_position[(i,byte_number)] = string
+    return strings_position
     file.close()
 
-    return strings_position
-
-
 info = [
-    "Text for tell.",
-    "Используйте кодировку utf-8.",
-    "Because there are 2 languages!",
-    "Спасибо!",
-]
+'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
 
-result = custom_write("test.txt", info)
-
+result = custom_write('test.txt', info)
 for elem in result.items():
-    print(elem)
+  print(elem)
+
+
