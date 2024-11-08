9d921402e1a5c310e64f44e48061c3511e638da9
Anna-Kuri <nankurenaisa@gmail.com>
2024-11-08 05:58:10 +0300

    module_9_1
    Module 9 hometask №1

---
+++ module_9_1.py
@@ -0,0 +1,14 @@
+from typing import Callable
+
+
+def apply_all_func(int_list: int | float, *functions: Callable) -> dict:
+    result = {}
+
+    for function in functions:
+        result[function.__name__] = function(int_list)
+
+    return result
+
+    
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
