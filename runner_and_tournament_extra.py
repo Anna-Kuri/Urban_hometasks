271600bc4722aed8997051c8251a0af8419a85b5
Anna-Kuri <nankurenaisa@gmail.com>
2024-12-20 01:16:48 +0300

    runner_and_tournament_extra
    Module 12 Hometask № 12 runner and tournament for extra task

---
+++ runner_and_tournament_extra.py
@@ -0,0 +1,43 @@
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants[:]:  # <- the change was made here
+                """The issue is that when we remove participants from the self.participants list just below (self.participants.remove(participant)) while looping through it, the list changes, and this can cause the loop to skip some participants by mistake.
+
+                To fix this, we can loop through a copy of the list (self.participants[:]) so the original list isn't changed while we're working with it.
+                """
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
