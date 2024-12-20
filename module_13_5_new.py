0c4c7bd0a7b9e3984a2281f7691610a5ade15da5
Anna-Kuri <nankurenaisa@gmail.com>
2024-12-20 15:32:29 +0300

    module_13_5_new
    Module 13 Hometask № 5 for  aiogram 3.15

---
+++ module_13_5_new.py
@@ -0,0 +1,103 @@
+import asyncio
+import logging
+from os import getenv
+import sys
+from dotenv import load_dotenv
+
+from aiogram.client.default import DefaultBotProperties
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+)
+
+load_dotenv()
+
+TOKEN = getenv("TELEGRAM_TOKEN")
+
+
+dp = Dispatcher()
+
+router = Router()
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@router.message(CommandStart())
+async def start(message) -> None:
+    await message.answer(
+        f"Привет! Я бот помогающий твоему здоровью.",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Рассчитать"),
+                    KeyboardButton(text="Информация"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+
+@router.message(F.text.casefold() == "calculate")
+async def set_age(message: Message, state: FSMContext):
+    await state.set_state(UserState.age)
+    await message.answer("Введите свой возраст:")
+
+
+@router.message(UserState.age)
+async def set_growth(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await state.set_state(UserState.growth)
+    await message.answer("Введите свой рост:")
+
+
+@router.message(UserState.growth)
+async def set_weight(message: Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await state.set_state(UserState.weight)
+    await message.answer("Введите свой вес:")
+
+
+@router.message(UserState.weight)
+async def send_calories(message: Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+
+    result = (
+        10 * int(data["weight"])
+        + 6.25 * int(data["growth"])
+        - 5 * int(data["age"])
+        - 161
+    )
+
+    await message.answer(f"Ваша норма калорий в сутки: {result}")
+
+    await state.clear()
+
+
+@router.message()
+async def all_messages(message) -> None:
+    await message.answer(f"Введите команду /start, чтобы начать общение.")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
